86a8308fc79cff88e5faa6413be28859805c978c
肉豆蔻 <1023876294@qq.com>
2024-01-18 01:02:54 +0800

    7.Condition接口: lock await signal

--- 大厂面试题第3季/src/main/java/com/atguigu/juc/LockSupportDemo.java
+++ 大厂面试题第3季/src/main/java/com/atguigu/juc/LockSupportDemo.java
@@ -1,9 +1,53 @@
 package com.atguigu.juc;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class LockSupportDemo {
     static Object objectLock = new Object();
+    static Lock lock = new ReentrantLock();
+    static Condition condition = lock.newCondition();
 
     public static void main(String[] args) {
+
+        new Thread(() -> {
+            // try {
+            //     TimeUnit.SECONDS.sleep(3L);
+            // } catch (InterruptedException e) {
+            //     throw new RuntimeException(e);
+            // }
+
+            lock.lock();
+            try {
+                // 必须在同步代码块中, 否则 java.lang.IllegalMonitorStateException
+                System.out.println(Thread.currentThread().getName() + "\t come in");
+                try {
+                    condition.await(); // 必须先await了才能signal
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName() + "\t 被唤醒");
+            } finally {
+                lock.unlock();
+            }
+        }, "A").start();
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+                // 必须在同步代码块中, 否则 java.lang.IllegalMonitorStateException
+                condition.signal();
+                System.out.println(Thread.currentThread().getName() + "\t 通知");
+            } finally {
+                lock.unlock();
+            }
+        }, "B").start();
+
+        // synchronizedWaitNotify();
+    }
+
+    private static void synchronizedWaitNotify() {
         new Thread(() -> {
             try {
                 Thread.sleep(3);
@@ -24,7 +68,7 @@ public class LockSupportDemo {
         new Thread(() -> {
             synchronized (objectLock) { // 必须在同步代码块中, 否则 java.lang.IllegalMonitorStateException
                 objectLock.notify();
-                System.out.println(Thread.currentThread().getName() + "\t通知");
+                System.out.println(Thread.currentThread().getName() + "\t 通知");
             }
         }, "B").start();
     }
