58ecd0828c3f6e97b51ca63f27acbba25a660e81
肉豆蔻 <1023876294@qq.com>
2024-01-18 01:39:24 +0800

    8.LockSupport类: park unpark

--- 大厂面试题第3季/src/main/java/com/atguigu/juc/LockSupportDemo.java
+++ 大厂面试题第3季/src/main/java/com/atguigu/juc/LockSupportDemo.java
@@ -1,7 +1,9 @@
 package com.atguigu.juc;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.LockSupport;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class LockSupportDemo {
@@ -11,6 +13,41 @@ public class LockSupportDemo {
 
     public static void main(String[] args) {
 
+        Thread t1 = new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + "\t come in\t" + System.currentTimeMillis());
+            // sleep方法3秒后醒来, 执行park无效, 没有阻塞效果, 解释如下
+            // 先执行了unpark(t1)导致上面park方法形同虚设无效, 时间一样
+            LockSupport.park();
+            // LockSupport.park();
+            System.out.println(Thread.currentThread().getName() + "\t 被唤醒\t" + System.currentTimeMillis());
+        }, "t1");
+        t1.start();
+
+        // try {
+        //     TimeUnit.SECONDS.sleep(3L);
+        // } catch (InterruptedException e) {
+        //     throw new RuntimeException(e);
+        // }
+
+        Thread t2 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t 通知");
+            LockSupport.unpark(t1);
+            // LockSupport.unpark(t1); 最多解开一个park, 因为permit最大值就是1, 调用多次都是1
+        }, "t2");
+        t2.start();
+
+
+        // lockAwaitSignal();
+
+        // synchronizedWaitNotify();
+    }
+
+    private static void lockAwaitSignal() {
         new Thread(() -> {
             // try {
             //     TimeUnit.SECONDS.sleep(3L);
@@ -43,8 +80,6 @@ public class LockSupportDemo {
                 lock.unlock();
             }
         }, "B").start();
-
-        // synchronizedWaitNotify();
     }
 
     private static void synchronizedWaitNotify() {
