b4d8d635d68e29a4517f414ac0056da90a3cd08b
肉豆蔻 <1023876294@qq.com>
2024-01-17 17:59:59 +0800

    2.两数之和算法

---
+++ 大厂面试题第3季/src/main/java/com/atguigu/javase/TwoSumDemo.java
@@ -0,0 +1,71 @@
+package com.atguigu.javase;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 两数之和: https://leetcode.cn/problems/two-sum/
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * <p>
+ * 你可以按任意顺序返回答案。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ * <p>
+ * 提示：
+ * <p>
+ * 2 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * 只会存在一个有效答案
+ */
+public class TwoSumDemo {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(new TwoSumDemo().twoSum1(nums, target)));
+        System.out.println(Arrays.toString(new TwoSumDemo().twoSum2(nums, target)));
+
+        nums = new int[]{3, 2, 4};
+        target = 6;
+        System.out.println(Arrays.toString(new TwoSumDemo().twoSum1(nums, target)));
+        System.out.println(Arrays.toString(new TwoSumDemo().twoSum2(nums, target)));
+    }
+
+    public int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{0};
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer , Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int key = target - nums[i];
+            if (map.containsKey(key)) {
+                return new int[]{map.get(key), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
