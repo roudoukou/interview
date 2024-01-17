9d45792e6d7c119bd30bf78fe0553de00ea9e23f
肉豆蔻 <1023876294@qq.com>
2024-01-17 22:21:25 +0800

    5.可重入锁-ReentrantLock显示锁

--- 大厂面试题第3季/src/main/java/com/atguigu/juc/ReEnterLockDemo.java
+++ 大厂面试题第3季/src/main/java/com/atguigu/juc/ReEnterLockDemo.java
@@ -1,8 +1,11 @@
 package com.atguigu.juc;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * 可重入锁: 可重复可递归调用的锁, 在外层使用锁之后, 在内层任然可以使用, 并且不发生死锁, 这样的锁就叫做可重入锁
- *
+ * <p>
  * 在一个synchronized修饰的方法或代码块的内部
  * 调用本类的其他synchronized修饰的方法或代码块, 是永远可以得到锁的
  */
@@ -26,6 +29,7 @@ public class ReEnterLockDemo {
     }
 */
 
+/*
     // 可重入锁-synchronized-同步方法
     private synchronized static void m1() {
         System.out.println("外层");
@@ -40,8 +44,35 @@ public class ReEnterLockDemo {
     private synchronized static void m3() {
         System.out.println("内层");
     }
+*/
+
+    static Lock lock = new ReentrantLock();
 
     public static void main(String[] args) {
-        m1();
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println("外层");
+                lock.lock();
+                try {
+                    System.out.println("内层");
+                } finally {
+                    lock.unlock();
+                }
+            } finally {
+                // 故意注释掉这行, 这里不释放锁, t2线程没法执行
+                // 由于加锁和释放次数不一样, 第二个线程始终无法获取到锁, 导致一直在等待
+                lock.unlock(); // 正常情况, 加锁几次, 就要解锁几次
+            }
+        }, "t1").start();
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + "调用开始");
+            } finally {
+                lock.unlock();
+            }
+        }, "t2").start();
     }
 }
