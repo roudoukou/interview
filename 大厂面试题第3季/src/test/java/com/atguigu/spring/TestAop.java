2302ce0944e9232f3907e5f3afefd645dbfcabf3
肉豆蔻 <1023876294@qq.com>
2024-01-21 21:11:55 +0800

    11.Spring5 AOP测试

--- 大厂面试题第3季/src/test/java/com/atguigu/spring/TestAop.java
+++ 大厂面试题第3季/src/test/java/com/atguigu/spring/TestAop.java
@@ -1,9 +1,7 @@
 package com.atguigu.spring;
 
 import com.atguigu.spring.service.CalcService;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.SpringVersion;
@@ -12,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import javax.annotation.Resource;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+// @RunWith(SpringRunner.class)
 public class TestAop {
     @Resource
     CalcService calcService;
@@ -39,15 +37,45 @@ public class TestAop {
         /*
         运行结果:
         Spring版本: 4.3.13.RELEASE
-        springboot版本: 1`.5.9.RELEASE
+        springboot版本: 1.5.9.RELEASE
         我是环绕通知之前AAA
         ********@Before我是前置通知
         ********@After我是后置通知
         ********@AfterThrowing我是异常通知
 
-        java.lang.ArithmeticException: / by zero`
+        java.lang.ArithmeticException: / by zero
          */
+    }
 
+    @Test
+    public void testAop5() {
+        System.out.println("Spring版本: " + SpringVersion.getVersion());
+        System.out.println("springboot版本: " + SpringBootVersion.getVersion());
 
+        // calcService.div(10, 2);
+        /*
+        运行结果:
+        Spring版本: 5.2.8.RELEASE
+        springboot版本: 2.3.3.RELEASE
+        我是环绕通知之前AAA
+        ********@Before我是前置通知
+        CalcServiceImpl被调用了, 我们的计算结果: 5
+        ********@AfterReturning我是返回后通知
+        ********@After我是后置通知
+        我是环绕通知之后BBB
+        */
+
+        calcService.div(10, 0);
+        /*
+        运行结果:
+        Spring版本: 5.2.8.RELEASE
+        springboot版本: 2.3.3.RELEASE
+        我是环绕通知之前AAA
+        ********@Before我是前置通知
+        ********@AfterThrowing我是异常通知
+        ********@After我是后置通知
+
+        java.lang.ArithmeticException: / by zero
+         */
     }
 }
