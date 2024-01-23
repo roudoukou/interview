8138ba9c8f07dcc938de84cc544bd914e2dce132
肉豆蔻 <1023876294@qq.com>
2024-01-23 22:29:04 +0800

    15.超卖程序 V1 搭建

---
+++ 大厂面试题第3季/boot_redis02/src/main/java/com/atguigu/boot_redis02/controller/GoodController.java
@@ -0,0 +1,33 @@
+package com.atguigu.boot_redis02.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GoodController {
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Value("${server.port}")
+    String serverPort;
+
+    @GetMapping("/buy_goods")
+    public String buy_goods() {
+        String result = stringRedisTemplate.opsForValue().get("goods:001");
+        int goodsNumber = result == null ? 0 : Integer.parseInt(result);
+
+        if (goodsNumber > 0) {
+            int realNumber = goodsNumber - 1;
+            stringRedisTemplate.opsForValue().set("goods:001", String.valueOf(realNumber));
+            System.out.println("成功买到商品, 库存还剩下: " + realNumber + " 件\t 服务提供端口: " + serverPort);
+
+            return "成功买到商品, 库存还剩下: " + realNumber + " 件\t 服务提供端口: " + serverPort;
+        } else {
+            System.out.println("商品已经售完/活动结束/调用超时,欢迎下次光临" + "\t服务提供端口" + serverPort);
+        }
+        return "商品已经售完/活动结束/调用超时,欢迎下次光临" + "\t服务提供端口" + serverPort;
+    }
+}
