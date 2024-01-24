bd58d4d6c3617929eda737e5168555fc9caa68ed
肉豆蔻 <1023876294@qq.com>
2024-01-24 14:22:05 +0800

    18.超卖程序 V4 finally自动释放分布式锁

--- 大厂面试题第3季/boot_redis02/src/main/java/com/atguigu/boot_redis02/controller/GoodController.java
+++ 大厂面试题第3季/boot_redis02/src/main/java/com/atguigu/boot_redis02/controller/GoodController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class GoodController {
@@ -21,27 +22,29 @@ public class GoodController {
 
     @GetMapping("/buy_goods")
     public String buy_goods() {
-        String value = UUID.randomUUID().toString() + Thread.currentThread().getName();
-        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, value);// setnx
+        try {
+            String value = UUID.randomUUID().toString() + Thread.currentThread().getName();
+            Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, value);// setnx
 
-        if (!flag) {
-            return "抢锁失败";
-        }
+            if (!flag) {
+                return "抢锁失败";
+            }
 
-        String result = stringRedisTemplate.opsForValue().get("goods:001");
-        int goodsNumber = result == null ? 0 : Integer.parseInt(result);
+            String result = stringRedisTemplate.opsForValue().get("goods:001");
+            int goodsNumber = result == null ? 0 : Integer.parseInt(result);
 
-        if (goodsNumber > 0) {
-            int realNumber = goodsNumber - 1;
-            stringRedisTemplate.opsForValue().set("goods:001", String.valueOf(realNumber));
-            System.out.println("成功买到商品, 库存还剩下: " + realNumber + " 件\t 服务提供端口: " + serverPort);
+            if (goodsNumber > 0) {
+                int realNumber = goodsNumber - 1;
+                stringRedisTemplate.opsForValue().set("goods:001", String.valueOf(realNumber));
+                System.out.println("成功买到商品, 库存还剩下: " + realNumber + " 件\t 服务提供端口: " + serverPort);
 
+                return "成功买到商品, 库存还剩下: " + realNumber + " 件\t 服务提供端口: " + serverPort;
+            } else {
+                System.out.println("商品已经售完/活动结束/调用超时,欢迎下次光临" + "\t服务提供端口" + serverPort);
+            }
+            return "商品已经售完/活动结束/调用超时,欢迎下次光临" + "\t服务提供端口" + serverPort;
+        } finally {
             stringRedisTemplate.delete(REDIS_LOCK);
-            return "成功买到商品, 库存还剩下: " + realNumber + " 件\t 服务提供端口: " + serverPort;
-        } else {
-            System.out.println("商品已经售完/活动结束/调用超时,欢迎下次光临" + "\t服务提供端口" + serverPort);
         }
-        return "商品已经售完/活动结束/调用超时,欢迎下次光临" + "\t服务提供端口" + serverPort;
-
     }
 }
