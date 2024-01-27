e3ef4270a3b86e0722dd53756376f3506b981bb3
肉豆蔻 <1023876294@qq.com>
2024-01-28 00:00:55 +0800

    leetcode

---
+++ leetcode/编程基础 0 到 1/src/main/java/icu/xiamu/class1672/Solution.java
@@ -0,0 +1,26 @@
+package icu.xiamu.class1672;
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        return solve1(accounts);
+    }
+
+    private int solve1(int[][] accounts) {
+        int accountMax = 0;
+        for (int i = 0; i < accounts.length; i++) {
+            int account = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                account += accounts[i][j];
+            }
+            accountMax = Math.max(accountMax, account);
+        }
+        return accountMax;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().maximumWealth(new int[][]{{1, 2, 3}, {3, 2, 1}})); // 6
+        System.out.println(new Solution().maximumWealth(new int[][]{{1, 5}, {7, 3}, {3, 5}})); // 10
+        System.out.println(new Solution().maximumWealth(new int[][]{{2, 8, 7}, {7, 1, 3}, {1, 9, 5}})); // 17
+    }
+}
