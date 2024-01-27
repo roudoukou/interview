94889bca58a2afd258a2b70e83345e2c435576d8
肉豆蔻 <1023876294@qq.com>
2024-01-27 21:12:48 +0800

    leetcode

---
+++ leetcode/编程基础 0 到 1/src/main/java/icu/xiamu/class709/Solution.java
@@ -0,0 +1,11 @@
+package icu.xiamu.class709;
+
+/**
+ * https://leetcode.cn/problems/to-lower-case/description/?envType=study-plan-v2&envId=programming-skills
+ */
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+}
