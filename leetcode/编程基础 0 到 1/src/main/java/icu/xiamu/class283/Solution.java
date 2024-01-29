f245b874b8960f7e9b795674acba1f5baa57c0e1
肉豆蔻 <1023876294@qq.com>
2024-01-29 23:00:50 +0800

    leetcode

--- leetcode/编程基础 0 到 1/src/main/java/icu/xiamu/class283/Solution.java
+++ leetcode/编程基础 0 到 1/src/main/java/icu/xiamu/class283/Solution.java
@@ -1,21 +1,31 @@
 package icu.xiamu.class283;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
     public void moveZeroes(int[] nums) {
+        solve1(nums);
+    }
 
-        List<Integer> list = new ArrayList<>();
+    private static void solve1(int[] nums) {
+        int count = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
-                list.add(i);
+                count++;
             }
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            
+        int [] result = new int[nums.length];
+        int index = 0;
+        for (int i = 0; i < nums.length ; i++) {
+            if (nums[i] != 0) {
+                result[index++] = nums[i];
+            }
         }
+
+        System.arraycopy(result, 0, nums, 0, nums.length);
     }
 
     public static void main(String[] args) {
