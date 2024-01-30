09d1d3ecd784d26243c77bc24b0c958f21eae75c
肉豆蔻 <1023876294@qq.com>
2024-01-31 00:29:45 +0800

    leetcode

---
+++ leetcode/LeetCode 75/src/main/java/icu/xiamu/class1768/Solution.java
@@ -0,0 +1,38 @@
+package icu.xiamu.class1768;
+
+public class Solution {
+    public String mergeAlternately(String word1, String word2) {
+
+        return solve1(word1, word2);
+
+    }
+
+    private String solve1(String word1, String word2) {
+
+        int length1 = word1.length();
+        int length2 = word2.length();
+        int minLength = Math.min(length1, length2);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < minLength; i++) {
+            stringBuilder.append(word1.charAt(i));
+            stringBuilder.append(word2.charAt(i));
+        }
+
+        if (length1 > length2) {
+            String substring = word1.substring(length2);
+            stringBuilder.append(substring);
+        } else if (length1 < length2){
+            String substring = word2.substring(length1);
+            stringBuilder.append(substring);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().mergeAlternately("abc", "pqr"));
+        System.out.println(new Solution().mergeAlternately("ab", "pqrs"));
+        System.out.println(new Solution().mergeAlternately("abcd", "pq"));
+    }
+}
