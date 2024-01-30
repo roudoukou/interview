b6e9fa082a242ea9c76ca31f3160fc1e507dd45b
肉豆蔻 <1023876294@qq.com>
2024-01-30 10:06:37 +0800

    leetcode

--- leetcode/编程基础 0 到 1/src/main/java/icu/xiamu/class283/Solution.java
+++ leetcode/编程基础 0 到 1/src/main/java/icu/xiamu/class283/Solution.java
@@ -4,9 +4,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * https://leetcode.cn/problems/move-zeroes/submissions/499233464/?envType=study-plan-v2&envId=programming-skills
+ */
 class Solution {
     public void moveZeroes(int[] nums) {
-        solve1(nums);
+        solve2(nums);
+
+        for (int num : nums) {
+            System.out.print(num + "\t");
+        }
+        System.out.println();
+    }
+
+    /**
+     * 0, 1, 0, 3, 12
+     * 1  1  0  3  12
+     * 1  3  0  3  12
+     * 1  3  12 3  12
+     * 1  3  12 0  0
+     * @param nums
+     */
+    private void solve2(int[] nums) {
+        int i = 0, j = 0, zeroCount = 0;
+        while (i != nums.length) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+
+            i++;
+        }
+        while (j != nums.length) {
+            nums[j] = 0;
+            j++;
+        }
     }
 
     private static void solve1(int[] nums) {
