654514417f5020c926397fd8f7c1a967cb497a48
肉豆蔻 <1023876294@qq.com>
2024-02-07 14:13:10 +0800

    leetcode

---
+++ leetcode/LeetCode 热题 100/src/main/java/icu/xiamu/class15/Solution.java
@@ -0,0 +1,42 @@
+package icu.xiamu.class15;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * https://leetcode.cn/problems/3sum/description/?envType=study-plan-v2&envId=top-100-liked
+ */
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        return solve1(nums);
+    }
+
+    private List<List<Integer>> solve1(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> ans = new HashSet<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i+1; j < nums.length - 1; j++) {
+                for (int k = j+1; k < nums.length; k++) {
+                    if ((nums[i] > 0 && nums[j] > 0 && nums[k] > 0) || (nums[i] < 0 && nums[j] < 0 && nums[k] < 0) ) {
+                        continue;
+                    }
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> list = Arrays.asList(nums[i], nums[j], nums[k]);
+                        Collections.sort(list);
+                        if (!ans.contains(list)) {
+                            // 让 nums[i], nums[j], nums[k] 有序
+                            ans.add(list);
+                        }
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(ans);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+        System.out.println(new Solution().threeSum(new int[]{0, 1, 1}));
+        System.out.println(new Solution().threeSum(new int[]{0, 0, 0}));
+    }
+}
