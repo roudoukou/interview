91a4d125795121e6fa59f95c5bcfe77b1af82ee6
肉豆蔻 <1023876294@qq.com>
2024-02-15 22:27:00 +0800

    4.Future缺点

---
+++ sgg-juc/src/main/java/icu/xiamu/juc/cf/FutureAPIDemo.java
@@ -0,0 +1,48 @@
+package icu.xiamu.juc.cf;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class FutureAPIDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        FutureTask<String> futureTask = new FutureTask<String>(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t come in");
+            TimeUnit.SECONDS.sleep(5);
+            return "task over";
+        });
+
+        Thread t1 = new Thread(futureTask);
+        t1.start();
+
+        // System.out.println(futureTask.get()); // 2.阻塞main线程
+        System.out.println(futureTask.get(3, TimeUnit.SECONDS)); // 3.设置阻塞超时时间
+        System.out.println(Thread.currentThread().getName() + "\t 忙其他任务了");
+        // System.out.println(futureTask.get()); // 1.不阻塞main线程
+    }
+}
+
+
+/*
+// 运行结果
+// 2.阻塞main线程
+Thread-0	 come in
+task over
+main	 忙其他任务了
+
+// 3.设置阻塞超时时间
+Thread-0	 come in
+Exception in thread "main" java.util.concurrent.TimeoutException
+	at java.util.concurrent.FutureTask.get(FutureTask.java:205)
+	at icu.xiamu.juc.cf.FutureAPIDemo.main(FutureAPIDemo.java:20)
+
+// 1.不阻塞main线程
+main	 忙其他任务了
+Thread-0	 come in
+task over
+
+总结: System.out.println(futureTask.get());
+1. get 容易导致阻寒，一般建议放在程序后面，一旦调用不见不做，非要等到结果才会离开，不管你是否计算完成，容易程序堵寒
+2. 假如我不愿意等待很长时间，我希望过时不候，可以自动离开.
+ */
