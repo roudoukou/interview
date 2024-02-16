77bb0a63b21c7a6bc36a05a6e43f3cdb5b427a04
肉豆蔻 <1023876294@qq.com>
2024-02-16 11:00:40 +0800

    4.Future缺点

--- sgg-juc/src/main/java/icu/xiamu/juc/cf/FutureAPIDemo.java
+++ sgg-juc/src/main/java/icu/xiamu/juc/cf/FutureAPIDemo.java
@@ -17,9 +17,20 @@ public class FutureAPIDemo {
         t1.start();
 
         // System.out.println(futureTask.get()); // 2.阻塞main线程
-        System.out.println(futureTask.get(3, TimeUnit.SECONDS)); // 3.设置阻塞超时时间
+        // System.out.println(futureTask.get(3, TimeUnit.SECONDS)); // 3.设置阻塞超时时间
         System.out.println(Thread.currentThread().getName() + "\t 忙其他任务了");
         // System.out.println(futureTask.get()); // 1.不阻塞main线程
+
+        // 4.通过轮询方式获取结果
+        while (true) {
+            if (futureTask.isDone()) {
+                System.out.println(futureTask.get());
+                break;
+            } else {
+                TimeUnit.MILLISECONDS.sleep(500);
+                System.out.println("正在处理中, 不要催了, 越催越慢, 再催熄火");
+            }
+        }
     }
 }
 
@@ -45,4 +56,19 @@ task over
 总结: System.out.println(futureTask.get());
 1. get 容易导致阻寒，一般建议放在程序后面，一旦调用不见不做，非要等到结果才会离开，不管你是否计算完成，容易程序堵寒
 2. 假如我不愿意等待很长时间，我希望过时不候，可以自动离开.
+
+// 4.通过轮询方式获取结果
+main	 忙其他任务了
+Thread-0	 come in
+正在处理中, 不要催了, 越催越慢, 再催熄火
+正在处理中, 不要催了, 越催越慢, 再催熄火
+正在处理中, 不要催了, 越催越慢, 再催熄火
+正在处理中, 不要催了, 越催越慢, 再催熄火
+正在处理中, 不要催了, 越催越慢, 再催熄火
+正在处理中, 不要催了, 越催越慢, 再催熄火
+正在处理中, 不要催了, 越催越慢, 再催熄火
+正在处理中, 不要催了, 越催越慢, 再催熄火
+正在处理中, 不要催了, 越催越慢, 再催熄火
+正在处理中, 不要催了, 越催越慢, 再催熄火
+task over
  */
