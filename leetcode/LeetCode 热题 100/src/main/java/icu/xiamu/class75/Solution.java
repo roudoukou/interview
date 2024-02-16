32398d44aa82afa99ebfdfcb480ed9e474bdc914
肉豆蔻 <1023876294@qq.com>
2024-02-16 12:55:29 +0800

    leetcode

---
+++ leetcode/LeetCode 热题 100/src/main/java/icu/xiamu/class75/Solution.java
@@ -0,0 +1,16 @@
+package icu.xiamu.class75;
+
+import java.util.Arrays;
+
+/**
+ *
+ * 75. 颜色分类
+ * https://leetcode.cn/problems/sort-colors/?envType=study-plan-v2&envId=top-100-liked
+ * 你说不用内置sort就不用?
+ * 就用就用
+ */
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+    }
+}
