1c10ae4b33c7525d0f87a52d04749e0a42502372
肉豆蔻 <1023876294@qq.com>
2024-02-17 14:26:48 +0800

    leetcode

---
+++ leetcode/LeetCode 热题 100/src/main/java/icu/xiamu/class138/Solution.java
@@ -0,0 +1,46 @@
+package icu.xiamu.class138;
+
+import icu.xiamu.common.ListNode;
+
+import java.util.Objects;
+
+/**
+ * 138. 随机链表的复制
+ * https://leetcode.cn/problems/copy-list-with-random-pointer/?envType=study-plan-v2&envId=top-100-liked
+ */
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        return process(head);
+    }
+
+    private Node process(Node head) {
+        Node current = new Node(-1);
+        Node newHead = current;
+
+        while (head != null) {
+
+            Node node = new Node(head.val);
+            node.random = head.random;
+            node.next = head.next;
+
+            head = head.next;
+            current = node;
+            current = current.next;
+        }
+
+        return newHead;
+    }
+}
