a2217553ba8e63fcef3b114c2f4828ef0abb3681
肉豆蔻 <1023876294@qq.com>
2024-02-23 21:57:22 +0800

    整合prometheus

---
+++ spring-demo/spring-prometheus/src/test/java/icu/xiamu/springprometheus/SpringPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.xiamu.springprometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrometheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
