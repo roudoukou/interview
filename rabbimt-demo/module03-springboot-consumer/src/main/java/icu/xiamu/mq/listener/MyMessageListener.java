796695843a5afea894c99ed48200309e72942408
肉豆蔻 <1023876294@qq.com>
2024-06-12 22:23:52 +0800

    5.mq 整合springboot

--- rabbimt-demo/module03-springboot-consumer/src/main/java/icu/xiamu/mq/listener/MyMessageListener.java
+++ rabbimt-demo/module03-springboot-consumer/src/main/java/icu/xiamu/mq/listener/MyMessageListener.java
@@ -16,12 +16,15 @@ public class MyMessageListener {
     public static final String EXCHANGE_DIRECT = "exchange.direct.order";  
     public static final String ROUTING_KEY = "order";  
     public static final String QUEUE_NAME  = "queue.order";  
-  
+
+    // 写法一: 监听 + 在RabbitMQ服务器上创建交换机, 队列
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(value = QUEUE_NAME, durable = "true"),
             exchange = @Exchange(value = EXCHANGE_DIRECT),
             key = {ROUTING_KEY}
     ))
+    // 写法二: 监听
+    // @RabbitListener(queues = {QUEUE_NAME})
     public void processMessage(String dateString,
                                Message message,
                                Channel channel) {
