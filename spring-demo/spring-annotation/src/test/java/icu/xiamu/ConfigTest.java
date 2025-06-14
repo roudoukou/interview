696d78dbbb248ec193122698dc3e438d153cd1b3
肉豆蔻 <1023876294@qq.com>
2025-06-14 15:06:44 +0800

    Scope注解使用

--- spring-demo/spring-annotation/src/test/java/icu/xiamu/ConfigTest.java
+++ spring-demo/spring-annotation/src/test/java/icu/xiamu/ConfigTest.java
@@ -1,7 +1,9 @@
 package icu.xiamu;
 
 import icu.xiamu.config.MyConfig;
+import icu.xiamu.config.MyConfig2;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -18,6 +20,33 @@ public class ConfigTest {
         for (String beanDefinitionName : beanDefinitionNames) {
             System.out.println(beanDefinitionName);
         }
+    }
+
+    /**
+     * 比较是否同一个对象
+     */
+    @Test
+    void test02() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(MyConfig2.class);
+        Object person1 = context.getBean("person");
+        Object person2 = context.getBean("person");
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person1 == person2);
+    }
+
+    /**
+     * prototype：多实例的：ioc容器启动并不会去调用方法创建对象放在容器中。
+     * 					每次获取的时候才会调用方法创建对象；
+     * singleton：单实例的（默认值）：ioc容器启动会调用方法创建对象放到ioc容器中。
+     * 			以后每次获取就是直接从容器（map.get()）中拿，
+     * request：同一次请求创建一个实例
+     * session：同一个session创建一个实例
+     */
+    @Test
+    void test03() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(MyConfig2.class);
+        // context.getBean("person");
     }
 
 }
