32e47f5f4def9e9b4a41add899a36f5dad081f8c
肉豆蔻 <1023876294@qq.com>
2025-06-21 16:00:27 +0800

    Bean生命周期，实现BeanPostProcessor接口，断点调试ApplicationContextAware

--- spring-demo/spring-annotation/src/main/java/icu/xiamu/entity/Dog.java
+++ spring-demo/spring-annotation/src/main/java/icu/xiamu/entity/Dog.java
@@ -1,5 +1,8 @@
 package icu.xiamu.entity;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -10,7 +13,10 @@ import javax.annotation.PreDestroy;
  * @date 2025/6/21
  */
 @Component
-public class Dog {
+public class Dog implements ApplicationContextAware {
+
+    ApplicationContext context;
+
     public Dog() {
         System.out.println("Dog constructor");
     }
@@ -26,5 +32,8 @@ public class Dog {
     }
 
 
-
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
 }
