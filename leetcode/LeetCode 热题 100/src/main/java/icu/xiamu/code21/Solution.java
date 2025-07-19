f64eea07090e70d1f41b835a6d34f29f7e9757d0
肉豆蔻 <1023876294@qq.com>
2025-07-19 15:54:29 +0800

    LeetCode 热题 100, 搜索二维矩阵

---
+++ leetcode/LeetCode 热题 100/src/main/java/icu/xiamu/code21/Solution.java
@@ -0,0 +1,25 @@
+package icu.xiamu.code21;
+
+/**
+ * <a href="https://leetcode.cn/problems/search-a-2d-matrix-ii/description/?envType=study-plan-v2&envId=top-100-liked">...</a>
+ * 240. 搜索二维矩阵 II
+ */
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().searchMatrix(new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 5));
+        System.out.println(new Solution().searchMatrix(new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 20));
+    }
+}
+
